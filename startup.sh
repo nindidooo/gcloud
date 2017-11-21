e2085ad1dc9191b0c8ffb5414f73f940ea3ea320
Matt Brown <matthewcsbrown@gmail.com>
2017-11-21 16:42:12 +1100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 mkdir /tmp/mediap
 cd /tmp/mediap
-# install packages to build python code and provide foreign function interface - DOCKER!!!!!
-sudo apt-get install python-dev build-essential autoconf libtool
+sudo apt-get -y install python-dev build-essential autoconf libtool
+
 wget -q \
-https://raw.githubusercontent.com/onsabimana/gcloud/master/requirements.txt
+https://raw.githubusercontent.com/nindidooo/gcloud/master/requirements.txt
+
 wget -q \
-https://raw.githubusercontent.com/onsabimana/gcloud/master/worker.py
+https://raw.githubusercontent.com/nindidooo/gcloud/master/worker.py
+
+wget -q \
+https://raw.githubusercontent.com/nindidooo/gcloud/master/makemidi.py
+
 curl https://bootstrap.pypa.io/get-pip.py | sudo python
-sudo pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-venv/bin/pip install -r requirements.txt
+
+sudo pip install -r requirements.txt
+
 python worker.py --subscription onsabimana_topic_subscription --dataset_id=onsabimana_media_processing --table_id=speech
