8f15ab1768cae7db09befc971ca0bdb493ae1344
Matt Brown <matthewcsbrown@gmail.com>
2017-11-21 16:42:56 +1100

    Update makemidi.py

--- makemidi.py
+++ makemidi.py
@@ -1,19 +1,21 @@
 from midiutil import MIDIFile
 
-degrees = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
-track = 0
-channel = 0
-time = 0    # In beats
-duration = 1    # In beats
-tempo = 60   # In BPM
-volume = 100  # 0-127, as per the MIDI standard
 
-MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
-# automatically)
-MyMIDI.addTempo(track, time, tempo)
+def create_midi(midi_filename="major-scale.mid"):
+    degrees = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+    track = 0
+    channel = 0
+    time = 0    # In beats
+    duration = 1    # In beats
+    tempo = 60   # In BPM
+    volume = 100  # 0-127, as per the MIDI standard
 
-for i, pitch in enumerate(degrees):
-    MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+    # automatically)
+    MyMIDI.addTempo(track, time, tempo)
 
-with open("major-scale.mid", "wb") as output_file:
-    MyMIDI.writeFile(output_file)
+    for i, pitch in enumerate(degrees):
+        MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+
+    with open(midi_filename, "wb") as output_file:
+        MyMIDI.writeFile(output_file)
