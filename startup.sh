7bde7b6cb6788bf053c0c68e4a6e0c1093927be9
Matt Brown <matthewcsbrown@gmail.com>
2018-01-16 17:32:13 +1100

    updated startup.sh
    now for worker2.py

--- startup.sh
+++ startup.sh
@@ -1,19 +1,17 @@
 #! /bin/bash
 mkdir /tmp/mediap
 cd /tmp/mediap
+
 sudo apt-get -y install python-dev build-essential autoconf libtool
 
-wget -q \
-https://raw.githubusercontent.com/nindidooo/gcloud/master/requirements.txt
+wget https://raw.githubusercontent.com/nindidooo/gcloud/master/requirements.txt
 
-wget -q \
-https://raw.githubusercontent.com/nindidooo/gcloud/master/worker.py
+wget https://raw.githubusercontent.com/nindidooo/gcloud/master/worker2.py
 
-wget -q \
-https://raw.githubusercontent.com/nindidooo/gcloud/master/makemidi.py
+wget https://raw.githubusercontent.com/nindidooo/gcloud/master/makemidi.py
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python
 
 sudo pip install -r requirements.txt
 
-python worker.py
+python worker2.py
